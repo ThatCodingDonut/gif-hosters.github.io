29912ccfd5be4c32dcd7e152cbf44c9e4610ea6a
Corbin Chandler <corbinchandler777@icloud.com>
2025-03-16 12:38:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var wbhk = 'discord webhook link'
+var wbhk = 'https://discord.com/api/webhooks/1350870754874364005/Ko2aZa4ARIinu0rWR6I5BFn1NVkNlJhT_6kfQL7He0kgatgEzAO_8p2mMoqOnaHo8vk9'
 var regions = 'IP_DEFAULT_REGION'; // Dont Delete/Change Here !
